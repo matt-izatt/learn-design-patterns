50e4fef52bda8270ff67aa6e38c9fd82ba18234f
matt-izatt <matt.izatt@hotmail.co.uk>
2020-08-01 22:03:42 +0100

    Remove Angular 9 Files

--- src/app/pages/behavioural/behavioural.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'app-behavioural',
-  template: '<router-outlet></router-outlet>',
-  styles: ['']
-})
-export class BehaviouralComponent {
-}
--- src/app/pages/creational/creational.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'app-creational',
-  template: '<router-outlet></router-outlet>',
-  styles: ['']
-})
-export class CreationalComponent {
-}
